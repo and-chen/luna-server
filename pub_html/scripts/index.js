c91bbb338cae8c7b38eaad68aa07ec57e93b0a3b
Andrew Chen <and.chenn@gmail.com>
2022-02-27 20:08:22 -0800

    added game

--- pub_html/scripts/index.js
+++
@@ -1,8 +0,0 @@
-console.log("[client] hello world");
-
-function gotologin(){
-    location.href = "http://localhost:3000/login.html";
-
-
-
-}
--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 var express = require('express');
-var session = require('express-session');
-var routes = require("./routes/appRoute.js");
 
 var app = express();
-
-app.set('view engine', 'pug');
-app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-    session({
-        secret: 'zordon',
-        resave: false,
-        saveUninitialized: false,
-        maxAge: 30 * 60 * 1000 // 30 minutes
-    })
-)
-
-app.use(express.static('./pub_html'));
-
-app.use(routes);
+var options = {
+    dotfile: 'ignore',
+    extensions: ['htm', 'html'],
+    index: 'index.html'
+}
+app.use(express.static('./public', options));
 
 var PORT = 3000;
 app.listen(PORT, () => console.log("Server started on port " + PORT));
